58cf190e0c72a6ccde51edcad0c587b0b826bbbf
Samuel Wu <samuel.wu@btnussfom0099.local>
2019-01-19 16:51:26 -0800

    Initial Commit

---
+++ Cfuncs.c
@@ -0,0 +1,13 @@
+//
+//  Cfuncs.c
+//  myLib
+//
+//  Created by Samuel Wu on 1/13/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#include "Cfuncs.h"
+float cmul(float a, float b)
+{
+  return a*b;
+}
